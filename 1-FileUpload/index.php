7f1f6a8bd80fa3d1c9276d2f5a8c468f283be986
Mr-Basirnia <mr.basirnia@gmail.com>
2021-12-29 22:26:40 +0330

    Add alert feature to upload file

--- 1-FileUpload/index.php
+++ 1-FileUpload/index.php
@@ -1,63 +1,94 @@
+<?php session_start(); ?>
+
 <!doctype html>
 <html lang="fa" dir="rtl">
 
 <head>
-   <!-- Required meta tags -->
-   <meta charset="utf-8">
-   <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-   <!-- Bootstrap CSS -->
-   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-      integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
-   <title>Hello, world!</title>
+    <title>Hello, world!</title>
 </head>
 
 <body>
-   <div class="container">
+    <div class="container">
 
-      <form action="upload.php" method="post" enctype="multipart/form-data">
+        <svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
+            <symbol id="check-circle-fill" fill="currentColor" viewBox="0 0 16 16">
+                <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z" />
+            </symbol>
+            <symbol id="info-fill" fill="currentColor" viewBox="0 0 16 16">
+                <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2z" />
+            </symbol>
+            <symbol id="exclamation-triangle-fill" fill="currentColor" viewBox="0 0 16 16">
+                <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
+            </symbol>
+        </svg>
+        <?php if (isset($_SESSION['uploadMsg']) && $_SESSION['uploadMsg']) : ?>
+            <div class="alert alert-success alert-dismissible fade show mt-5" role="alert">
 
-         <div class="row g-3">
+                <strong>تبریک میگم</strong> فایل شما با موفقیت آپلود شد
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
 
-            <div class="col-12">
+                <svg class="bi flex-shrink-0 float-start" width="24" height="24" role="img" aria-label="Success:">
+                    <use xlink:href="#check-circle-fill" />
+                </svg>
+            </div>
+            <?php unset($_SESSION['uploadMsg']); ?>
+        <?php endif ?>
 
-               <div class="p-5">
+        <?php if (isset($_SESSION['uploadMsg']) && !$_SESSION['uploadMsg']) : ?>
+            <div class="alert alert-danger alert-dismissible fade show mt-5" role="alert">
 
-                  <label for="formFileLg" class="form-label">فایل مورد نظر خود را انتخاب کنید</label>
-                  <input class="form-control form-control-lg" id="formFileLg" type="file" name="fileInput">
+                <strong>متاسفم</strong> مشکلی بوجود اومده لطفا دوباره تلاش کنید
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
 
-               </div>
+                <svg class="bi flex-shrink-0 float-start" width="24" height="24" role="img" aria-label="Danger:">
+                    <use xlink:href="#exclamation-triangle-fill" />
+                </svg>
+            </div>
+            <?php unset($_SESSION['uploadMsg']); ?>
+        <?php endif ?>
+        <form action="upload.php" method="post" enctype="multipart/form-data">
+
+            <div class="row g-3">
+
+                <div class="col-12">
+
+                    <div class="p-5">
+
+                        <label for="formFileLg" class="form-label">فایل مورد نظر خود را انتخاب کنید</label>
+                        <input class="form-control form-control-lg" id="formFileLg" type="file" name="fileInput">
+
+                    </div>
+
+                </div>
+
+                <div class="col-12">
+                    <div class="px-5">
+                        <button type="submit" class="btn btn-outline-primary me-auto d-block" name="fileBtn">آپلود
+                            فایل</button>
+                    </div>
+                </div>
 
             </div>
 
-            <div class="col-12">
-               <div class="px-5">
-                  <button type="submit" class="btn btn-outline-primary me-auto d-block" name="fileBtn">آپلود
-                     فایل</button>
-               </div>
-            </div>
+        </form>
+    </div>
 
-         </div>
+    <!-- Optional JavaScript; choose one of the two! -->
 
-      </form>
-   </div>
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
-   <!-- Optional JavaScript; choose one of the two! -->
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
 
-   <!-- Option 1: Bootstrap Bundle with Popper -->
-   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-      integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
-      crossorigin="anonymous"></script>
-
-   <!-- Option 2: Separate Popper and Bootstrap JS -->
-
-   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"
-      integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB"
-      crossorigin="anonymous"></script>
-   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"
-      integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13"
-      crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
 
 </body>
 
