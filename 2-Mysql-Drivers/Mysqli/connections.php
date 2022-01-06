8182272545c1b5f4433fb4e804250a28ea76b52a
Mr-Basirnia <mr.basirnia@gmail.com>
2022-01-06 14:32:32 +0330

    Complete the database connection

--- 2-Mysql-Drivers/Mysqli/connections.php
+++ 2-Mysql-Drivers/Mysqli/connections.php
@@ -4,10 +4,16 @@ $mysqli = new mysqli(
     'localhost',
     'root',
     '',
-    'test'
+    'world'
 );
 
 if ($mysqli->connect_errno) {
     echo 'There was an error connecting to the database'. $mysqli->connect_error;
     exit;
 }
+
+# اگر اتصال به دیتابیس اوکی بود کد های زیر اجرا می شود
+
+echo 'connections is successfully';
+
+$mysqli->set_charset('utf8');
